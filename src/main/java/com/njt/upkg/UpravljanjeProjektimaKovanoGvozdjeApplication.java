498806751c4e560df4dadf2926a842bd9e35aba5
Aleksa Djordjiev <aleksa.kb24@live.com>
2023-07-03 14:04:02 +0200

    Initial commit

---
+++ src/main/java/com/njt/upkg/UpravljanjeProjektimaKovanoGvozdjeApplication.java
@@ -0,0 +1,13 @@
+package com.njt.upkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpravljanjeProjektimaKovanoGvozdjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpravljanjeProjektimaKovanoGvozdjeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/njt/upkg/UpravljanjeProjektimaKovanoGvozdjeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.njt.upkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpravljanjeProjektimaKovanoGvozdjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
